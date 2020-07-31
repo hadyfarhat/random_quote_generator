61af733e89488e77dcef4d697a8561fa2d564e4f
Hadi <hadyfarhat97@gmail.com>
2020-07-31 21:01:19 +0100

    quotes array

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,45 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /*** 
  * `quotes` array 
 ***/
-
-
+var quotes = [
+    {
+        quote: "The problem you see, it is easy. Most eyes can see what the player can do, but few eyes can see the effect the player has.",
+        source: "Andrea Pirlo"
+    },
+    {
+        quote: "God gives gifts to everyone. Some can write. Some can sing. He gave me the skill to play football and I'm making the most of it.",
+        source: "Ronaldinho"
+    },
+    {
+        quote: "In most scenarios it isn't the man on the ball who decides where the ball goes, but players without the ball. Their running actions determine the next pass.",
+        source: "Johan Cruyff"
+    },
+    {
+        quote: "Goals are only important if they win games.",
+        source: "Lionel Messi"
+    },
+    {
+        quote: "Your love makes me strong. Your hate makes me unstoppable.",
+        source: "Cristiano Ronaldo"
+    },
+    {
+        quote: "Football is played in the head. Your feet are just the tools.",
+        source: "Andrea Pirlo"
+    },
+    {
+        quote: "If you train badly, you play badly. If you work like a beast in training, you play the same way.",
+        source: "Pep Guardiola"
+    },
+    {
+        quote: "I've never played for a draw in my life.",
+        source: "Sir Alex Ferguson",
+        citation: "Alex Ferguson: My Autobiography",
+        year: 2013
+    }
+]
 
 /***
  * `getRandomQuote` function
